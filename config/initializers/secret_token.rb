4339cb40c853fc5a3d872f4661fe1e8a64aca751
Leonid Batizhevsky <the.leonko@gmail.com>
2012-05-25 15:25:06 +0400

    code generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VacationCalendar::Application.config.secret_token = 'fd21e81b380e315a84472db48d59f1924a02f950a595e808127afee62960fc5f1a227bdbe87d308ce561e4201881892b3706e16c8d7c042d2d21490d71477abc'
